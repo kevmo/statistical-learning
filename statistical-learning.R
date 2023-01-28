85052441c53e3feeef93452782e3a5c93a352f22
Kevin Moore <k.james.moore@gmail.com>
2023-01-28 16:58:26 -0500

    College data set init

---
+++ statistical-learning.R
@@ -0,0 +1,73 @@
+#
+#
+# BASIC COMMANDS
+#
+#
+
+# create a vector x
+x <- c(1,2,3,4)
+
+# ?funcname for info
+?c
+
+y = c(1,2,3)
+
+length(x)
+
+
+# ls() lists all objects saved so far
+ls()
+
+rm(x)
+
+ls()
+
+
+
+#
+#
+# GRAPHICS
+#
+#
+
+
+
+
+
+
+#
+#
+# INDEXING DATA
+#
+#
+
+
+
+
+
+
+#
+#
+# LOADING DATA
+#
+#
+
+
+
+
+
+#
+#
+# ADDITIONAL GRAPHICAL AND NUMERICAL SUMMARIES
+#
+#
+
+
+
+
+
+
+
+#
+#
+#
